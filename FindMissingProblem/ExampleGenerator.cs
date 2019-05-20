2fbc84ca090e414e9067442e040cf61f25632e52
Luke Glasgow <lukeglasgow@hotmail.co.uk>
2019-05-20 21:33:25 +0100

    Add project files.

---
+++ FindMissingProblem/ExampleGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMissingProblem
+{
+    public class ExampleGenerator
+    {
+        public int[] FullSet { get; set; }
+        public int[] MissingValueSet { get; set; }
+
+        public Random random { get; set; }
+
+        public ExampleGenerator()
+        {
+            random = new Random();
+        }
+
+        public void CreateExampleSets()
+        {
+            
+            var arraySize = random.Next(4, 20);
+
+            FullSet = new int[arraySize];
+            MissingValueSet = new int[arraySize - 1];
+
+            for (int i = 0; i < FullSet.Length; i++)
+            {
+                FullSet[i] = GetUniqueValue(random);
+            }
+
+            var indexToRemove = random.Next(0, FullSet.Length);
+
+            var fullList = FullSet.ToList<int>();
+            fullList.RemoveAt(indexToRemove);
+            MissingValueSet = fullList.ToArray();
+
+            //int missingSetIndex = 0;
+            //for (int k = 0; k < FullSet.Length; k++)
+            //{
+            //    if (k == indexToRemove)
+            //        continue;
+
+            //    MissingValueSet[missingSetIndex] = FullSet[k];
+            //    missingSetIndex += 1;
+            //}
+
+            ScramblePartialArray(random);
+        }
+
+        private int GetUniqueValue(Random rand)
+        {
+            var val = rand.Next(0, 100);
+            while (FullSet.Contains(val))
+            {
+                val = rand.Next(0, 100);
+            }
+
+            return val;
+        }
+
+        private void ScramblePartialArray(Random r)
+        {
+            var array = MissingValueSet;
+            var iterations = r.Next(1000, 2500);
+            
+            for (int i = 0; i < iterations; i++)
+            {
+                var index = r.Next(0, array.Length - 1);
+                var temp = array[index];
+                try
+                {
+                    array[index] = array[index + 1];
+                    array[index + 1] = temp;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    array[index] = array[index - 1];
+                    array[index - 1] = temp;
+                }         
+            }
+
+            MissingValueSet = array;
+        }
+
+
+    }
+}
---
+++ FindMissingProblem/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMissingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleGenerator eg = new ExampleGenerator();
+
+            do
+            {
+                Console.Clear();
+                eg.CreateExampleSets();
+                
+                Console.WriteLine($"The full set is:");
+                PrintArray(eg.FullSet);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"The set with missing value is:");
+                PrintArray(eg.MissingValueSet);
+
+                Console.WriteLine();
+
+                Console.WriteLine($"The missing value is: {FindMissingHash(eg.FullSet, eg.MissingValueSet)}");
+
+                Console.WriteLine();
+
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write("Application closing.");
+            System.Threading.Thread.Sleep(700);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(700);
+            Console.Write(".");
+            System.Threading.Thread.Sleep(700);
+            
+            Environment.Exit(0);
+        }
+
+        static void PrintArray(int[] array)
+        {
+            var output = "[ ";
+
+            foreach (int i in array)
+                output += $"{i} ";
+
+            Console.WriteLine( output += ']' );
+        }
+
+
+        // Finds missing when arrays are in the same order
+        static int FindMissing(int[] full, int[] partial)
+        {
+            int missingValue = -1;
+
+            //var partialIndex = 0;
+            for (int i = 0; i < full.Length; i++)
+            {
+                try
+                {
+                    if (full[i] == partial[i])
+                        continue;
+
+                    return full[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    missingValue = full[i];
+                }
+            }
+
+            return missingValue;
+        }
+
+        static int FindMissingHash(int[] full, int[] partial)
+        {
+            int missingValue = -1;
+
+            var fullDict = full
+                .Select((value, index) => new { value, index })
+                .ToDictionary(i => i.index, i => i.value);
+
+            var partialDict = partial
+                .Select((value, index) => new { value, index })
+                .ToDictionary(i => i.index, i => i.value);
+
+            foreach (int val in fullDict.Values)
+            {
+                if (!partialDict.ContainsValue(val))
+                    missingValue = val;
+
+                else continue;
+            }
+
+            return missingValue;
+        }
+
+        static void PrintDictionary(Dictionary<int, int> dict)
+        {
+            if (dict.Count == 0 || dict == null)
+                return;
+
+            var output = "[ ";
+
+            for (int i = 0; i < dict.Count; i++)
+            {
+                if (i == (dict.Count - 1))
+                {
+                    output += $"{i} : {dict[i]} ]";
+                    continue;
+                }
+                output += $"{i} : {dict[i]}, ";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
